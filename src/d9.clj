0c07c7409b685437711b716c2a11ddf43d0faf40
Brandon White <brandon.ray.white@gmail.com>
2022-12-09 18:01:53 -0700

    Day 7 signum

--- src/d9.clj
+++ src/d9.clj
@@ -1,27 +1,19 @@
 (ns d9
   (:require
    [clojure.java.io :as io]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [criterium.core :refer [bench]]))
 
 ;(def input (slurp (io/resource "d9ex.txt")))
 ;(def input (slurp (io/resource "d9ex-2.txt")))
 (def input (slurp (io/resource "d9.txt")))
 
-(def knot-len 1)
-(def stretch-limit (* knot-len (Math/sqrt 2)))
-
-(def ->direction
-  {"U" :up
-   "D" :down
-   "L" :left
-   "R" :right})
-
 (defn ->step
   [s]
   (let [[c d] (str/split s #" ")]
-    (repeat (parse-long d) (->direction c))))
+    (repeat (parse-long d) c)))
 
-(def kw-steps
+(def raw-steps
   (->> (str/split input #"\n")
        (map ->step)
        (apply concat)))
@@ -30,80 +22,54 @@
   [head direction]
   (let [[x y] head]
     (cond
-      (= direction :up)    [x (inc y)]
-      (= direction :down)  [x (dec y)]
-      (= direction :left)  [(dec x) y]
-      (= direction :right) [(inc x) y])))
+      (= direction "U") [x (inc y)]
+      (= direction "D") [x (dec y)]
+      (= direction "L") [(dec x) y]
+      (= direction "R") [(inc x) y])))
 
-(defn kw-step-reducer
-  [acc kw-step]
-  (conj acc (move-head (last acc) kw-step)))
+(defn step-reducer
+  [acc step]
+  (conj acc (move-head (last acc) step)))
 
-(def initial-head-steps
-  (reduce kw-step-reducer [[0 0]] kw-steps))
+(def head-steps
+  (reduce step-reducer [[0 0]] raw-steps))
 
-(defn neighbors
-  [coords]
-  (let [[x y] coords]
-    [[x (+ y knot-len)] ; N
-     [x (- y knot-len)] ; S
-     [(+ x knot-len) y] ; E
-     [(- x knot-len) y] ; W
-     [(- x knot-len) (+ y knot-len)] ; NW
-     [(+ x knot-len) (+ y knot-len)] ; NE
-     [(- x knot-len) (- y knot-len)] ; SW
-     [(+ x knot-len) (- y knot-len)] ; SE
-     ]))
-(memoize neighbors)
-
-(defn distance
-  [node-1 node-2]
-  (let [[x1 y1] node-1
-        [x2 y2] node-2]
-    (Math/sqrt
-     (+
-      (Math/pow (abs (- x2 x1)) 2)
-      (Math/pow (abs (- y2 y1)) 2)))))
-(memoize distance)
-
-(defn closest-neighbor
-  [coords neighbors]
-  (->>
-   (map-indexed
-    (fn [_ itm]
-      [itm (distance coords itm)]) neighbors)
-   (sort-by second)
-   (ffirst)))
+(defn next-tail
+  [head tail]
+  (let [diff (map - head tail)]
+    (when (> (apply max (map abs diff)) 1)
+      (map + tail (map #(Integer/signum %) diff)))))
+(def memo-next-tail (memoize next-tail))
 
 (defn tail-steps
-  [coll]
+  [head-steps]
   (loop
-   [steps (rest coll)
-    tail (first coll)
+   [steps (rest head-steps)
+    tail (first head-steps)
     visited [tail]]
     (if (empty? steps)
       visited
-      (let [head (first steps)]
-        (if (> (distance head tail) stretch-limit)
-          (let [tail (closest-neighbor head (neighbors tail))]
-            (recur (rest steps) tail (conj visited tail)))
-          (recur (rest steps) tail visited))))))
+      (if-let [tail (memo-next-tail (first steps) tail)]
+        (recur (rest steps) tail (conj visited tail))
+        (recur (rest steps) tail visited)))))
 
 ;; Part one result
-(->> initial-head-steps
+(->> head-steps
     (tail-steps)
     (set)
     (count))
 
 (defn nth-tail-steps
-  [n steps]
-  (loop [steps steps nth 0]
+  [n head-steps]
+  (loop [steps head-steps nth 0]
     (if (= n nth)
       steps
       (recur (tail-steps steps) (inc nth)))))
 
 ;; Part two result
-(->> initial-head-steps
+;(bench
+(->> head-steps
     (nth-tail-steps 9)
      (set)
      (count))
+;)
